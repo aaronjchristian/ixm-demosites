e842dbb75fab67975d646856b9c15411ea492028
Aaron Christian <aaronc@imagexmedia.com>
2021-07-05 15:38:16 -0700

    Added BS5 theme, updated the accordion component.

---
+++ docroot/themes/custom/swat4/js/scripts.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Simple helpers for the swat4 theme.
+ */
+
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
+  /**
+   * Core/Misc behaviours.
+   */
+  Drupal.behaviors.swat4Misc = {
+    attach: function (context, settings) {
+      console.log('here');
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
